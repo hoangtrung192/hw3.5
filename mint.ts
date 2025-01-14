9bbcf4c56e5ba3d5c75044d3064c8a26653d3578
Duc Long <longspeed001@gmail.com>
2025-01-14 21:40:39 +0700

    mint

---
+++ mint.ts
@@ -0,0 +1,38 @@
+import {
+    ForgeScript,
+    MeshTxBuilder,
+    resolveScriptHash,
+    stringToHex,
+} from "@meshsdk/core";
+import { blockchainProvider, wallet } from "./test_provider";
+async function main() {
+    const changeAddress = await wallet.getChangeAddress();
+    const forgingScript = ForgeScript.withOneSignature(changeAddress); //tao 1 script ki duy nhat cho nguoi ki
+    const txBuilder = new MeshTxBuilder({
+        fetcher: blockchainProvider,
+        submitter: blockchainProvider,
+        verbose: true, //true -> thong tin chi tiet ve giao dich
+    });
+    const utxos = await wallet.getUtxos();
+    const demo = {
+        name: "NFT demo",
+        image: "ipfs://QmRzicpReutwCkM6aotuKjErFCUD213DpwPq6ByuzMJaua",
+        mediaType: "image/jpg",
+        description: "mint by mesh and long",
+    };
+    const policyID = resolveScriptHash(forgingScript); //chuyen doi 1 cai script thanh policyID dinh danh
+    const tokenName = "NFT demo";
+    const tokenNameHex = stringToHex(tokenName);
+    const metadata = { [policyID]: { [tokenName]: { ...demo } } };
+    const unsignedTx = await txBuilder
+        .mint("1", policyID, tokenNameHex)
+        .mintingScript(forgingScript)
+        .metadataValue(721, metadata) //chuan theo cardano
+        .changeAddress(changeAddress)
+        .selectUtxosFrom(utxos)
+        .complete();
+    const signedTx = await wallet.signTx(unsignedTx);
+    const txHash = await wallet.submitTx(signedTx);
+}
+main();
+//npx tsx mint.ts
