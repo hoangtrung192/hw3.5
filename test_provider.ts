0a412aa59d09d2952278c6675cd153e75f0aef06
Duc Long <longspeed001@gmail.com>
2025-01-14 20:34:20 +0700

    push

--- test_provider.ts
+++ test_provider.ts
@@ -10,7 +10,7 @@ const wallet = new MeshWallet({
     fetcher: blockchainProvider, // Provider để truy vấn blockchain
     submitter: blockchainProvider, // Provider để gửi giao dịch
     key: {
-        type: 'mnemonic',
+        type: 'mnemonic', // loai 24 ki tu
         words: [
             "heart", "outdoor", "element", "clinic", "mushroom", 
             "clap", "undo", "author", "clip", "upper", "silk", 
@@ -20,6 +20,17 @@ const wallet = new MeshWallet({
         ], // Danh sách các từ mnemonic
     },
 });
+
 console.log("Dia chi cua vi : ", wallet.getChangeAddress());
-fs.writeFileSync('owner.addr', wallet.getChangeAddress());
+// fs.writeFileSync('owner.addr', wallet.getChangeAddress());
+async function main() {
+  
+    // Lấy danh sách các địa chỉ chưa sử dụng
+    const balance =  wallet.getBalance();
+    console.log("Số dư ví :  ", balance);
+}
+
+main().catch((error) => {
+    console.error("Đã xảy ra lỗi:", error);
+});
 //npx tsx test_provider.ts
